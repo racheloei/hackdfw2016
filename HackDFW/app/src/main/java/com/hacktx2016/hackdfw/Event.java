4c4bdec8e4ef12d4194f5511081b1f3ae8d06226
Rachel Oei <racheloei@users.noreply.github.com>
2016-04-16 13:00:05 -0500

    Event.java created with toString

---
+++ HackDFW/app/src/main/java/com/hacktx2016/hackdfw/Event.java
@@ -0,0 +1,27 @@
+public class Event {
+    private String title;
+    private String description;
+    private int rating;
+    
+    public Event(String t, String d, int r) {
+        title = t;
+        description = d;
+        rating = r;
+    }
+    
+    public String toString() {
+        return title + "\n" + rating + " stars\n" + description + "\n\n";
+    }
+    
+    public String getTitle() {
+        return title;
+    }
+    
+    public String getDescription() {
+        return description;
+    }
+    
+    public int getRating() {
+        return rating;
+    }
+}
